6d431fdfe2e81c40d973c2ff2b589520d31ba99c
Fitrakun <fitrakun@gmail.com>
2018-10-04 17:36:59 +0900

    Connect authentication with password salt in S3

--- handlers/auth/auth.py
+++ handlers/auth/auth.py
@@ -5,10 +5,26 @@ import tornado
 import tornado.locale
 import hashlib
 import os
+import boto3 
+import botocore
 
 from handlers.base import BaseHandler
+from boto3 import Session
 
-AuthURL = os.path.join(os.path.dirname('./..'), "static/members/")
+# AWS S3 Configuration
+BUCKET_NAME = 'chec-static'
+session = Session()
+credentials = session.get_credentials()
+current_credentials = credentials.get_frozen_credentials()
+s3 = boto3.resource('s3')
+s3c = boto3.client('s3',aws_access_key_id=current_credentials.access_key,aws_secret_access_key=current_credentials.secret_key,aws_session_token=current_credentials.token)
+
+# AWS S3 access bucket
+myBucket = s3.Bucket(BUCKET_NAME)
+config = s3c._client_config
+config.signature_version = botocore.UNSIGNED
+
+# AuthURL = os.path.join(os.path.dirname('./..'), "static/members/")
 
 #Sign In
 class SignInHandler(BaseHandler):
@@ -21,9 +37,20 @@ class SignInHandler(BaseHandler):
         password = self.get_argument("password", default="")
         #Query
         md5_password = hashlib.md5(password.encode("utf-8")).hexdigest()
-        memberList = [ x for x in os.listdir(AuthURL)]
-        if username in memberList:
-            with open(AuthURL + username + '/password.salt') as f:
+        isUser = False
+        AuthURL = ""
+
+        for file in myBucket.objects.filter(Prefix="members-180615/"+username, Delimiter = '\\'):
+            if 'password.salt' in file.key:
+                dir = os.path.dirname(file.key)
+                if not os.path.exists(dir):
+                    os.makedirs(dir)
+                s3.Bucket(BUCKET_NAME).download_file(file.key, file.key)
+                isUser = True
+                AuthURL = file.key
+
+        if isUser == True:
+            with open(AuthURL) as f:
                 pwd = f.read()
             if md5_password == pwd:
                 self.message = ""
@@ -35,6 +62,21 @@ class SignInHandler(BaseHandler):
         else:
             self.message = "Username hasn't been signed up!"
             self.redirect('/signin')
+
+        # memberList = [ x for x in os.listdir(AuthURL)]
+        # if username in memberList:
+        #     with open(AuthURL + username + '/password.salt') as f:
+        #         pwd = f.read()
+        #     if md5_password == pwd:
+        #         self.message = ""
+        #         self.set_secure_cookie('user', username)
+        #         self.redirect('/')
+        #     else:
+        #         self.message = "Wrong Password!"
+        #         self.redirect('/signin')
+        # else:
+        #     self.message = "Username hasn't been signed up!"
+        #     self.redirect('/signin')
 
 #Sign Out
 class SignOutHandler(BaseHandler):
