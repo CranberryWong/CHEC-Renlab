b31f697575e945c943ea3300b12f822815745df8
fitrakun <fitrakun@gmail.com>
2020-12-04 11:23:23 +0900

    Add scroll to top button

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -560,4 +560,20 @@ $(document).ready(function() {
         })
     }
 
+    //back to top
+        $(window).scroll(function () {
+                if ($(this).scrollTop() > 50) {
+                    $('#back-to-top').fadeIn();
+                } else {
+                    $('#back-to-top').fadeOut();
+                }
+            });
+            // scroll body to 0px on click
+            $('#back-to-top').click(function () {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 400);
+                return false;
+            });
+
 });
