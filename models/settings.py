52cb16d041b88647014a8e6726f778ba98d4182c
xfzhu2008 <zxf900703@gmail.com>
2021-02-22 20:40:12 +0900

    Update settings.py

--- models/settings.py
+++ models/settings.py
@@ -5,7 +5,7 @@ DBSETTINGS = dict(
    db_name = 'postgres',
    db_host = 'xrenlabdb.cc3wskflz0nc.ap-northeast-1.rds.amazonaws.com',
    db_user = 'postgres',
-   db_password = 'root',
+   db_password = 'renlab10',
    db_port = 5432,
    db_encode = 'utf8',
 )
