ecb0ba34043b6912c52af528a051a7128cbe8129
xfzhu2008 <zxf900703@gmail.com>
2021-11-10 15:06:54 +0900

    1

--- models/settings.py
+++ models/settings.py
@@ -3,9 +3,9 @@
 
 DBSETTINGS = dict(
    db_name = 'postgres',
-   db_host = 'localhost',
-   db_user = 'workuser',
-   db_password = 'santana0120',
+   db_host = 'xrenlabdb.cc3wskflz0nc.ap-northeast-1.rds.amazonaws.com',
+   db_user = 'postgres',
+   db_password = 'renlab10',
    db_port = 5432,
    db_encode = 'utf8',
 )
